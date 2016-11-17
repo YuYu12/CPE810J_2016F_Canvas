262fc45e1eca8db33d74d646cf446119c5ae36f9
YuYu12 <yyu17@stevens.edu>
2016-11-17 17:07:15 -0500

    Test Data

---
+++ PlagiarismDetector/src/Test1.java
@@ -0,0 +1,12 @@
+package Plagiarism;
+
+public class Test1 {
+	public Test1(){
+		System.out.println("hehe");
+
+	}
+	public int hehe(){
+		return 0;
+	}
+	
+}
---
+++ PlagiarismDetector/src/Test2.java
@@ -0,0 +1,12 @@
+package Plagiarism;
+
+public class Test2 {
+	public Test2(){
+		System.out.println("hehe");
+
+	}
+	public int hehe(){
+		return 0;
+	}
+	
+}
